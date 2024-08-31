7f6f412e610e10b9d7980962383201d4eeae8a2f
navinbarangeengagebay <navin@engagebay.co>
2024-08-31 18:13:39 +0530

    changes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -22,17 +22,17 @@ export default async function Blog() {
   return (
     <main className="max-w-7xl mx-auto md:px-16 px-6">
       <PageHeading
-        title="Blog"
-        // description="Welcome to my blog domain where I share personal stories about things I've learned, projects I'm hacking on and just general findings. I also write for other publications."
+        title="Blogs"
+        description="Welcome to my blog where I share personal stories about things I've learned, I also write for other publications."
       >
-        {/* <Social type="publication" /> */}
+         <Social type="publication" />
       </PageHeading>
 
       <Slide delay={0.1}>
-        {/* <div className="flex items-center gap-x-3 mb-8">
-          <BiDetail />
+         <div className="flex items-center gap-x-3 mb-8">
+          {/*<BiDetail />*/}
           <h2 className="text-xl font-semibold tracking-tight">Explore All</h2>
-        </div> */}
+        </div>
         <Posts />
       </Slide>
     </main>
